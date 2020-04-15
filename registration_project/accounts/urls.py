295c3cd7fd6b2b12e01b3793cf6876c0d3a6420b
deep-sarkar <deepsarkar96@gmail.com>
2020-04-15 18:46:16 +0530

    [Deep]Add.added Login url

--- registration_project/accounts/urls.py
+++ registration_project/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterAPIView, activate
+from .views import RegisterAPIView, activate, LoginAPIView
 
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('activate/<surl>/', activate, name='activate'),
+    path('login/',LoginAPIView.as_view(), name='login'),
 ]

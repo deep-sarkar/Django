a154267b4d076feec8a246d8c27aadd00df403a5
deep-sarkar <deepsarkar96@gmail.com>
2020-04-15 18:51:46 +0530

    [Deep]Add.added Reset password url path

--- registration_project/accounts/urls.py
+++ registration_project/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterAPIView, activate, LoginAPIView
+from .views import RegisterAPIView, activate, LoginAPIView, ChangePassword
 
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('activate/<surl>/', activate, name='activate'),
     path('login/',LoginAPIView.as_view(), name='login'),
+    path('changepassword/', ChangePassword.as_view(), name='changepassword'),
 ]

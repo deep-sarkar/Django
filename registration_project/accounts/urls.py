8745025ada513815c6872c892e1190c37962fb92
deep-sarkar <deepsarkar96@gmail.com>
2020-04-15 18:35:36 +0530

    [Deep]Add.added token activation url path

--- registration_project/accounts/urls.py
+++ registration_project/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterAPIView
+from .views import RegisterAPIView, activate
 
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
+    path('activate/<surl>/', activate, name='activate'),
 ]

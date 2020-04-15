a33a7e8774fdb7c0cc602edc1495683edbaed62c
deep-sarkar <deepsarkar96@gmail.com>
2020-04-15 17:59:41 +0530

    [Deep]Add.added Registration serializer

---
+++ registration_project/accounts/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+
+User = get_user_model()
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password        = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    password2       = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'password2',
+        ]
+        extra_kwargs = {'password':{'write_only':True}}
+        required_fields = fields

291d797955900079bad854221a80dea081945197
deep-sarkar <deepsarkar96@gmail.com>
2020-04-17 20:18:05 +0530

    [Deep]Refactor.modifies logout url

--- registration_project/accounts/urls.py
+++ registration_project/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (RegisterAPIView, activate, LoginAPIView, 
-                LogoutView, ChangePassword, Forgotpassword, reset_password, ActivateNewPassword, Home )
+                logout, ChangePassword, Forgotpassword, reset_password, ActivateNewPassword, Home )
 
 
 urlpatterns = [
@@ -8,7 +8,7 @@ urlpatterns = [
     path('activate/<surl>/', activate, name='activate'),
     path('login/',LoginAPIView.as_view(), name='login'),
     path('changepassword/', ChangePassword.as_view(), name='changepassword'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', logout, name='logout'),
     path('forgotpassword/', Forgotpassword.as_view(), name='forgotpassword'),
     path('resetpassword/<surl>/',reset_password, name='resetpassword'),
     path('activatenewpassword/<user_reset>/',ActivateNewPassword.as_view(), name='activatenewpassword'),

3a22c66a776aea5bc0a4074a055d41202575b7dd
deep-sarkar <deepsarkar96@gmail.com>
2020-04-17 18:09:28 +0530

    [Deep]Add.added default seting for redis

--- registration_project/registration_project/settings.py
+++ registration_project/registration_project/settings.py
@@ -149,3 +149,13 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'wotbsingh@gmail.com'
 EMAIL_HOST_PASSWORD = '9950618403'
 EMAIL_USE_SSL = False
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}

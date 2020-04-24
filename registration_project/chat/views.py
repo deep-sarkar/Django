de558b671c4c4d9dc34c9213370fee3211a6fa07
deep-sarkar <deepsarkar96@gmail.com>
2020-04-24 15:14:26 +0530

    [Deep]Add.added create_room method

--- registration_project/chat/views.py
+++ registration_project/chat/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.http import JsonResponse
+from .models import Room, RoomMember
+import json
 
-def index(request):
-    return render(request, 'chat/index.html', {})
+def chat_home(request):
+	return render(request, 'chat/chat_home.html')
 
-def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name': room_name
-    })
+def create_room(request):
+	room_name = request.POST.get('room_name')
+	username = request.user.username
+	room = Room.objects.filter(room_name=room_name)
 
+	if room is None:
+		Room.objects.create(room_name=room_name, owner=username)
+		RoomMember.objects.create(room_name=room_name, member=username, isAuthenticated = True)
+		return render(request, 'accounts/home.html')
+
+	else:
+		return render(request, 'chat/room_error_message.html',{
+			'message':'Room eith this name already exists, please try with different name'
+		})

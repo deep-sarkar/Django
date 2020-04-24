8e789377b2123350938fca0e99951cde1e20854c
deep-sarkar <deepsarkar96@gmail.com>
2020-04-24 15:13:53 +0530

    [Deep]Add.added Room and Room member models

--- registration_project/chat/models.py
+++ registration_project/chat/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
-from django.contrib.auth.models import User
+class Room(models.Model):
+	room_name  = models.CharField(max_length=20, blank=False, null=False)
+	owner    = models.EmailField()
 
+	def __str__(self):
+		return self.owner
 
-class Message(models.Model):
-    user      = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user', related_name='user', db_index=True)
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='recipient', related_name='recipient', db_index=True)
-    timestamp = models.DateTimeField(auto_now_add=True, db_index = True)
-    body      = models.TextField(max_length=500)
+class RoomMember(models.Model):
+	room_name  = models.CharField(max_length=20, blank=False, null=False)
+	member = models.EmailField(null=False, blank=False)
+	isAuthorized = models.BooleanField()
 
-    def __str__(self):
-        return self.user
+	def __str__(self):
+		return self.member

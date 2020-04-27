95f66dafec2eda6db522d2b571d49d1ce8e54228
deep-sarkar <deepsarkar96@gmail.com>
2020-04-27 11:12:35 +0530

    [Deep]Add.added urls for chat app

--- registration_project/chat/urls.py
+++ registration_project/chat/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import path
-
-from .views import chat_home, create_room
+from .views import create_room, delete_room, chat_home, approve_request, join_room, pending_request
 
 urlpatterns = [
     path('', chat_home, name='chat_home'),
-    path('create_room/', create_room, name='create_room'),
+    path('create_room/',create_room, name='create_room'),
+    path('delete_room/', delete_room, name='delete_room'),
+    path('join_room/', join_room, name='join_room'),
+    path('pending_request/', pending_request, name='pending_request'),
+    path('approve_request/', approve_request, name='approve_request'),
 ]

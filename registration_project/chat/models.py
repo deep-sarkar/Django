83fe5fe0ae41a2a697e945bbab16ab8878d7b697
deep-sarkar <deepsarkar96@gmail.com>
2020-04-27 11:07:37 +0530

    [Deep]Add.added Group and group member models

--- registration_project/chat/models.py
+++ registration_project/chat/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
-class Room(models.Model):
-	room_name  = models.CharField(max_length=20, blank=False, null=False)
-	owner    = models.EmailField()
+
+class GroupChat(models.Model):
+	owner	  = models.CharField(max_length=50)
+	room_name = models.CharField(max_length=20)
+	
 
 	def __str__(self):
-		return self.owner
+		return self.room_name
 
-class RoomMember(models.Model):
-	room_name  = models.CharField(max_length=20, blank=False, null=False)
-	member = models.EmailField(null=False, blank=False)
-	isAuthorized = models.BooleanField()
+class GroupMembers(models.Model):
+	room_name	 = models.CharField(max_length=20)
+	member 	  	 = models.CharField(max_length=20)
+	isAuthorised = models.BooleanField(default=False)
 
 	def __str__(self):
-		return self.member
+		return self.room_name

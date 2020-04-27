b1ab4726a7bb65ea5720e59c42533d1350586ed1
deep-sarkar <deepsarkar96@gmail.com>
2020-04-27 11:09:20 +0530

    [Deep]Add.added chat home and create room method

--- registration_project/chat/views.py
+++ registration_project/chat/views.py
@@ -1,22 +1,29 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Room, RoomMember
+
+from . models import GroupChat, GroupMembers
 import json
 
 def chat_home(request):
-	return render(request, 'chat/chat_home.html')
+    return render(request, 'chat/chat_home.html')
+
 
 def create_room(request):
 	room_name = request.POST.get('room_name')
 	username = request.user.username
-	room = Room.objects.filter(room_name=room_name)
+	queryset = GroupChat.objects.filter(room_name=room_name)
 
-	if room is None:
-		Room.objects.create(room_name=room_name, owner=username)
-		RoomMember.objects.create(room_name=room_name, member=username, isAuthenticated = True)
-		return render(request, 'accounts/home.html')
+	if queryset.count() == 0:
+		chat_room = GroupChat.objects.create(room_name=room_name, owner=username)
+		chat_room_members = GroupMembers.objects.create(room_name=room_name, member=username, isAuthorised = True)
+		chat_room.save()
+		chat_room_members.save()
+		return render(request, 'chat/room_general_message.html',{
+			'message':'Room created. Please go back and join room'
+		})
 
 	else:
-		return render(request, 'chat/room_error_message.html',{
-			'message':'Room eith this name already exists, please try with different name'
+		return render(request, 'chat/room_general_message.html',{
+			'message':'Room with this name already exists, please try with different name'
 		})
+

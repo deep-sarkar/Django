4d580fe3f0bddaab88ba0abbd55a993e83d514c0
deep-sarkar <deepsarkar96@gmail.com>
2020-04-30 21:03:04 +0530

    [Deep]Add.added chatroom method

--- registration_project/p2pchat/views.py
+++ registration_project/p2pchat/views.py
@@ -46,3 +46,34 @@ def get_stream(request):
     return JsonResponse(response_data)
 
 
+def chat_room(request, stream):
+    username = request.user.username
+    print("username",username)
+    stream_name = stream
+    print(stream_name)
+    return render(request, "p2p/chat_room.html",{
+        "stream":stream_name
+    })
+
+# def save_message(request):
+#     username     = request.user.username
+#     body_data    = json.loads(request.body.decode('utf-8'))
+#     user         = body_data['user']
+#     message      = body_data['message']
+
+
+
+# def get_message(request, receiver):
+#     username  = request.user.username
+#     queryset = Message.objects.filter(sender=username, receiver=receiver)
+#     if queryset.count() != 1:
+#         queryset = Message.objects.filter(sender=receiver, receiver=username)
+#     stream = queryset[0].sender + queryset[0].receiver
+#     all_message = [query.message for chat, query in enumerate(queryset)]
+    
+#     response_data = {
+#         'data': all_message,
+#         'stream': stream
+#     }
+#     return JsonResponse(response_data)
+

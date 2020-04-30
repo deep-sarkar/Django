fe9c876ee8e9616bd48d50d923e0971bcc89a275
deep-sarkar <deepsarkar96@gmail.com>
2020-04-30 20:45:48 +0530

    [Deep]Refactor.modified routing path

--- registration_project/registration_project/routing.py
+++ registration_project/registration_project/routing.py
@@ -1,11 +1,15 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
+import chat.consumers 
+import p2pchat.consumers
+
+from django.urls import re_path
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter([
+            re_path(r'ws/chat/(?P<room_name>\w+)/$', chat.consumers.ChatConsumer),
+            re_path(r'ws/chat/(?P<stream>\w+)/$', chat.consumers.ChatConsumer),
+        ])
     ),
 })

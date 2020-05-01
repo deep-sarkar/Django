784f06101aa0a3cc93672419ccccc90f83e050a3
deep-sarkar <deepsarkar96@gmail.com>
2020-05-01 17:40:15 +0530

    [Deep]Refactor.modified stream related fields

--- registration_project/p2pchat/views.py
+++ registration_project/p2pchat/views.py
@@ -28,52 +28,28 @@ def get_stream(request):
     body_data    = json.loads(request.body.decode('utf-8'))
     receiver     = body_data['receiver']
     queryset     = Message.objects.filter(sender=username, receiver=receiver)
-    if queryset.count() != 1:
-        queryset = Message.objects.filter(sender=receiver, receiver=username)
-        if queryset.count() != 1:
-            message_obj = Message.objects.create(sender=username, receiver=receiver, body="chat")
-            stream = str(username)+str(receiver)
+    if queryset.count() == 0:
+        queryset     = Message.objects.filter(sender = receiver, receiver = username)
+        if queryset.count() == 0:
+            stream = username + receiver
+            message_obj = Message.objects.create(stream=stream, sender=username, receiver=receiver,  body="chat")
             message_obj.save() 
-        else:
-            stream = str(queryset[0].sender) + str(queryset[0].receiver)
-    else:
-        print("hello")
-        stream = str(queryset[0].sender) + str(queryset[0].receiver)
-        print(stream)
+            response_data = {
+            'data': stream
+            }
+            return JsonResponse(response_data)
+    stream = queryset[0].stream
     response_data = {
-        'data': stream
-    }
+            'data': stream
+        }
     return JsonResponse(response_data)
 
 
 def chat_room(request, stream):
-    username = request.user.username
-    print("username",username)
     stream_name = stream
-    print(stream_name)
     return render(request, "p2p/chat_room.html",{
         "stream":stream_name
     })
 
-# def save_message(request):
-#     username     = request.user.username
-#     body_data    = json.loads(request.body.decode('utf-8'))
-#     user         = body_data['user']
-#     message      = body_data['message']
 
-
-
-# def get_message(request, receiver):
-#     username  = request.user.username
-#     queryset = Message.objects.filter(sender=username, receiver=receiver)
-#     if queryset.count() != 1:
-#         queryset = Message.objects.filter(sender=receiver, receiver=username)
-#     stream = queryset[0].sender + queryset[0].receiver
-#     all_message = [query.message for chat, query in enumerate(queryset)]
-    
-#     response_data = {
-#         'data': all_message,
-#         'stream': stream
-#     }
-#     return JsonResponse(response_data)
 

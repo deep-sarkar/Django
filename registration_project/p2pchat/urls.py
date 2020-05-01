24f71ba929908ef477f86a1a5d8707ac7827fd3c
deep-sarkar <deepsarkar96@gmail.com>
2020-05-01 17:42:42 +0530

    [Deep]Add.added save message endpoint

--- registration_project/p2pchat/urls.py
+++ registration_project/p2pchat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import p2phome, contact_list, get_stream,chat_room
+from .views import p2phome, contact_list, get_stream,chat_room,save_message
 
 urlpatterns =[
     path('', p2phome, name='p2phome'),
     path('contact_list/', contact_list, name='contact_list'),
     path('get_stream/', get_stream, name='get_stream'),
-    path('chat_room/<stream>', chat_room, name="chat_room")
+    path('chat_room/<stream>', chat_room, name="chat_room"),
+    path('save_message/', save_message, name='save_message')
 ]

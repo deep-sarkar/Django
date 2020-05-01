13fda930d3c9dcaed1fb0bccaf3ebfc94105dcc4
deep-sarkar <deepsarkar96@gmail.com>
2020-05-01 17:39:29 +0530

    [Deep]Refactor.modified model structure by adding stream

--- registration_project/p2pchat/models.py
+++ registration_project/p2pchat/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class Message(models.Model):
+    stream    = models.CharField(max_length=50, blank=False, null=False) 
     sender    = models.CharField(max_length=20, blank=False, null=False)
     receiver  = models.CharField(max_length=20, blank=False, null=False)
     body      = models.CharField(max_length=250)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.sender
+        return self.stream
 
 class Meta:
     ordering = ['-timestamp']

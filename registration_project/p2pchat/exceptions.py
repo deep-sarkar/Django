785dad94d7a70c74e506d5c7919cb0e9f6d7f572
deep-sarkar <deepsarkar96@gmail.com>
2020-05-02 10:23:53 +0530

    [Deep]Add.added EmptyContactListError

--- registration_project/p2pchat/exceptions.py
+++ registration_project/p2pchat/exceptions.py
@@ -1,3 +1,7 @@
 class EmptyMessageError(Exception):
     def __init__(self,msg):
         super().__init__(msg)
+
+class EmptyContactListError(Exception):
+    def __init__(self,msg):
+        super().__init__(msg)

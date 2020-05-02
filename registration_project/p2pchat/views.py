7e49a6c6f620681e60b40c6e904ea0cec24768dc
deep-sarkar <deepsarkar96@gmail.com>
2020-05-02 10:23:11 +0530

    [Deep]Refactor.handeled jsonDecoderError

--- registration_project/p2pchat/views.py
+++ registration_project/p2pchat/views.py
@@ -23,9 +23,9 @@ def p2phome(request):
     return render(request, 'p2p/home.html')
 
 def contact_list(request):
-    username     = request.user.username
-    queryset = User.objects.exclude(username=username)
-    all_contacts = [item.username for name, item in enumerate(queryset)]
+    username      = request.user.username
+    queryset      = User.objects.exclude(username=username)
+    all_contacts  = [item.username for name, item in enumerate(queryset)]
     response_data = {
 		'data': all_contacts
     }

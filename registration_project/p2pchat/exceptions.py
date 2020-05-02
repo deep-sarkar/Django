bb07e2a5101e842cf9f1ac8085c177388ad7db69
deep-sarkar <deepsarkar96@gmail.com>
2020-05-02 10:38:54 +0530

    [Deep]Add.added doc string for exception details

--- registration_project/p2pchat/exceptions.py
+++ registration_project/p2pchat/exceptions.py
@@ -1,7 +1,13 @@
+'''
+Exception will raised when message will be empty
+'''
 class EmptyMessageError(Exception):
     def __init__(self,msg):
         super().__init__(msg)
 
+'''
+Exception will raised when user does not have any contact.
+'''
 class EmptyContactListError(Exception):
     def __init__(self,msg):
         super().__init__(msg)
